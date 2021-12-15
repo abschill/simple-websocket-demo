2c2a0385b9e45dcb049af8df82ab24ccf281ae26
abschill <ajay198810@gmail.com>
2021-12-15 07:01:57 -0500

    rm comment

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const WebSocket = require( 'ws' );
 const wss = new WebSocket.WebSocketServer({ port: 8080 });
 
 wss.on( 'connection', ( ws ) => {
-    // console.log( )
+
     ws.on( 'message', ( data ) => {
         console.log( 'received: %s', data );
-        wss.clients.forEach(function each( client ) {
-           // console.log( client );
+        wss.clients.forEach( ( client ) => {
+
             if ( client.readyState === WebSocket.OPEN ) {
               client.send( data.toString() );
             }
           });
     });
 
-    ws.on( 'close', ( data ) => {
-        wss.clients.forEach(function each( client ) {
-            // console.log( client );
+    ws.on( 'close', _ => {
+        wss.clients.forEach(( client ) => {
              if ( client.readyState === WebSocket.OPEN ) {
                client.send( 'a user left' );
              }
